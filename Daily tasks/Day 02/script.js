962946baafd59019b44c263e9342b6be2abbea80
justinabosconz <justinabosconz@gmail.com>
2025-12-19 11:50:45 +1300

    Day 02

---
+++ Daily tasks/Day 02/script.js
@@ -0,0 +1,28 @@
+//1
+const myVariable = "Justina Bosco";
+const age = 39;
+let isStudent = true;
+let favProgramingLanguage = "Javascript";
+
+//2
+console.log(myVariable, age, isStudent, favProgramingLanguage);
+
+//3
+// myVariable = "Tapas"; Unable to reassign
+isStudent = false;
+
+//4
+let obj = {
+  name: "Justina",
+  years: 39,
+};
+
+let arr = [
+  {
+    myName: "Justina",
+    myAge: 39,
+  },
+];
+
+obj.name = "Amitabh";
+arr = [1, 2, 3, 4];
